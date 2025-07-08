71743f6fe516c3a88002cb68940bc8041b763141
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-07-08 13:47:11 -0300

    Criando a documentacao com o swagger e integrando ao Express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import devRoutes from "./routes/devRoutes";
 import dotenv from "dotenv";
+import swaggerDocument from "./docs/swagger.json";
+import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
 
@@ -9,8 +11,10 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/dev", devRoutes);
-
+if (process.env.NODE_ENV === "development") {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 app.listen(port, () => {
-  console.log("Servidor está rodando....")
+  console.log("Servidor está rodando....");
   console.log(`Server rodando em http://localhost:${port}`);
 });
