661de8ae18902ad24ba1448dfc31f3026788ae50
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-07-10 16:38:17 -0300

    refatorando os controladores para usar RequestHandler do Express

--- src/controllers/devControllers.ts
+++ src/controllers/devControllers.ts
@@ -1,17 +1,23 @@
-import { Request, Response } from "express";
+import { Request, Response, RequestHandler } from "express";
 import devService from "../services/devServices";
 import { devSchema } from "../schemas/devSchema";
 
-export async function getAllDevs(req: Request, res: Response) {
+export const getAllDevs: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
   try {
     const devs = await devService.getAll();
     res.json(devs);
   } catch (error) {
     res.status(500).json({ error: "Erro interno do servidor" });
   }
-}
+};
 
-export async function getDevById(req: Request, res: Response) {
+export const getDevById: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
   try {
     const dev = await devService.getById(Number(req.params.id));
     if (!dev) res.status(404).send("Dev não encontrado");
@@ -19,39 +25,49 @@ export async function getDevById(req: Request, res: Response) {
   } catch (error) {
     res.status(500).json({ error: "Erro interno do servidor" });
   }
-}
+};
 
-export async function createDev(req: Request, res: Response) {
+export const createDev: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
   const validation = devSchema.safeParse(req.body);
 
   if (!validation.success) {
-    return res.status(400).json({
+     res.status(400).json({
       error: "Dados inválidos",
       details: validation.error.errors,
     });
+    return;
   }
 
   const newDev = await devService.add(validation.data);
-  res.status(201).json(newDev);
-}
+   res.status(201).json(newDev);
+};
 
-export async function updateDev(req: Request, res: Response) {
+export const updateDev: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
   const dev = await devService.getById(Number(req.params.id));
-  if (!dev) res.status(404).send("Dev não encontrado");
+  if (!dev)  res.status(404).send("Dev não encontrado");
 
   const validation = devSchema.partial().safeParse(req.body);
 
   if (!validation.success) {
-    return res.status(400).json({
+     res.status(400).json({
       error: "Dados inválidos",
       details: validation.error.errors,
     });
   }
   const updatedDev = await devService.update(Number(req.params.id), req.body);
-  res.status(201).json(updatedDev);
-}
+   res.status(201).json(updatedDev);
+};
 
-export async function deleteDev(req: Request, res: Response) {
+export const deleteDev: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
   try {
     const dev = await devService.getById(Number(req.params.id));
     if (!dev) res.status(404).send("Dev não encontrado");
@@ -60,4 +76,4 @@ export async function deleteDev(req: Request, res: Response) {
   } catch (error: any) {
     res.status(400).json({ error: error.message });
   }
-}
+};
