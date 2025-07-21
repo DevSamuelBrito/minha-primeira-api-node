e3d2bd094e6cfc804ca4374823bf4f94efb5809e
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-07-21 17:02:44 -0300

    feat: atualiza retorno para incluir dados de devs e projetos

--- src/services/devServices.ts
+++ src/services/devServices.ts
@@ -1,38 +1,116 @@
+import { Dev, Project } from "@prisma/client";
 import devRepository from "../repositories/devRepository";
-import Dev from "../models/devModel";
+import {
+  CreateDevInput,
+  CreateProjectInput,
+  UpdateProjectInput,
+} from "../models/devModel";
 
 class DevService {
-  async getAll(): Promise<Dev[]> {
-    return devRepository.getAll();
+
+  //GET
+  async getAllDevs(): Promise<Dev[]> {
+    const devs = await devRepository.getAllDevs();
+
+    if (!devs || devs.length === 0) {
+      throw new Error("Nenhum desenvolvedor encontrado");
+    }
+    return devs;
   }
 
-  async getById(id: number): Promise<Dev | null> {
-    return devRepository.getById(id);
+  async getAllDevsByID(id: string): Promise<Dev | null> {
+    const devs = await devRepository.getDevById(id);
+
+    if (!devs) {
+      throw new Error(`Nenhum desenvolvedor com o ID ${id} encontrado`);
+    }
+    return devs;
   }
 
-  async add(dev: Omit<Dev, "id">): Promise<Dev> {
-    if (!dev.nome) throw new Error("Nome é obrigatório");
-    if (!dev.linguagem) throw new Error("Linguagem é obrigatório");
-    return devRepository.add(dev);
+  async getAllProjects(): Promise<Project[] | null> {
+    const projetcs = await devRepository.getAllProjects();
+
+    if (!projetcs || projetcs.length === 0) {
+      throw new Error("Nenhum projeto encontrado");
+    }
+    return projetcs;
   }
 
-  async update(
-    id: number,
-    data: Partial<Omit<Dev, "id">>
-  ): Promise<Dev | null> {
-    const devAtual = await this.getById(id);
-    if (!devAtual) throw new Error("Dev não encontrado");
+  async getProjectByID(id: string): Promise<Project | null> {
+    const projetcs = await devRepository.getProjectById(id);
 
-    const nome = data.nome ?? devAtual.nome;
-    const linguagem = data.linguagem ?? devAtual.linguagem;
-    
-    return devRepository.update(id, { nome, linguagem });
+    if (!projetcs) {
+      throw new Error(`Nenhum projeto com o ID ${id} encontrado`);
+    }
+    return projetcs;
   }
 
-  async delete(id: number): Promise<Dev | null> {
-    const dev = await this.getById(id)
-    if (!dev) throw new Error("Dev não encontrado");
-    return devRepository.delete(id);
+  //POST
+  async createDev(data: CreateDevInput) {
+    if (!data.name) throw new Error("Nome é obrigatório");
+    if (!data.techs) throw new Error("Linguagem é obrigatória");
+
+    const newDev = await devRepository.createDev(data);
+    if (!newDev) throw new Error("Erro ao criar desenvolvedor");
+    return newDev;
+  }
+
+  async createProject(data: CreateProjectInput) {
+    if (!data.name) throw new Error("Nome do projeto é obrigatório");
+    if (!data.description)
+      throw new Error("Descrição do projeto é obrigatória");
+    if (
+      !data.devProjects ||
+      data.devProjects.length === 0 ||
+      !data.devProjects[0].devId
+    )
+      throw new Error("ID do desenvolvedor é obrigatório");
+
+    const newProject = await devRepository.createProject(data);
+    if (!newProject) throw new Error("Erro ao criar projeto");
+    return newProject;
+  }
+
+  //PUT
+  async updateDev(id: string, data: Partial<CreateDevInput>) {
+    if (!id) throw new Error("ID do desenvolvedor é obrigatório");
+    if (!data) throw new Error("Dados para atualizar são obrigatórios");
+
+    const existingDev = await devRepository.getDevById(id);
+    if (!existingDev)
+      throw new Error(`Desenvolvedor com ID ${id} não encontrado`);
+
+    const updateDev = await devRepository.updateDev(data, id);
+    if (!updateDev) throw new Error("Erro ao atualizar desenvolvedor");
+    return updateDev;
+  }
+
+  async updateProject(id: string, data: Partial<UpdateProjectInput>) {
+    if (!id) throw new Error("ID do desenvolvedor é obrigatório");
+    if (!data) throw new Error("Dados para atualizar são obrigatórios");
+
+    const existingProject = await devRepository.getProjectById(id);
+    if (!existingProject){
+      throw new Error(`Projeto com ID ${id} não encontrado`);
+    }
+    const updateProject = await devRepository.updateProject(data, id);
+    if (!updateProject) throw new Error("Erro ao atualizar projeto");
+    return updateProject;
+  }
+
+  //Delete 
+  async deleteDev(id: string){
+    if(!id) throw new Error("ID do desenvolvedor é obrigatório");
+    const existingDev = await devRepository.getDevById(id);
+    if (!existingDev) throw new Error(`Desenvolvedor com ID ${id} não encontrado`);
+
+    const deleteDev = await devRepository.deleteDev(id);
+    if (!deleteDev) throw new Error("Erro ao deletar desenvolvedor");
+    return deleteDev;
+  }
+
+  async deleteProject(id: string){
+
   }
 }
 
