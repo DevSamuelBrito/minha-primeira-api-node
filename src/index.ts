8ec93eccaa745f0c31bc86c9009f2728b8848fde
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-07-21 16:54:27 -0300

    chore: adiciona  dependência cors e configura cors globalmente

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import devRoutes from "./routes/devRoutes";
 import dotenv from "dotenv";
 import swaggerDocument from "./docs/swagger.json";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ export const app = express();
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/dev", devRoutes);
 if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
