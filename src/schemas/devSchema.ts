472bfcb89e5152284c35de73250938ac49fa52e2
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-07-27 20:38:56 -0300

    feat: define schemas para criação e update de dev e project

--- src/schemas/devSchema.ts
+++ src/schemas/devSchema.ts
@@ -1,6 +1,60 @@
-import { z } from "zod";
+import { string, z } from "zod";
 
-export const devSchema = z.object({
-    nome: z.string().min(1, "Nome é obrigatório"),
-    linguagem: z.string().min(1, "Linguagem é obrigatória"),
-})
+export const createDevSchema = z.object({
+  name: z.string().min(1, "Nome é obrigatório"),
+  email: z.string().email("Email inválido"),
+  techs: z
+    .array(string().min(1, "Tecnologia não pode ser vazia"))
+    .min(1, "Pelo menos uma tecnologia é obrigatória"),
+
+  //devProjects é opcional aqui
+  devProjects: z
+    .array(
+      z.object({
+        projectId: z.string().min(1, "O ID do projeto é obrigatório"),
+      })
+    )
+    .optional(),
+});
+
+export const updateDevSchema = z.object({
+  name: z.string().min(1, "Nome é obrigatório").optional(),
+  email: z.string().email("Email inválido").optional(),
+  techs: z
+    .array(string().min(1, "Tecnologia não pode ser vazia"))
+    .min(1, "Pelo menos uma tecnologia é obrigatória")
+    .optional(),
+  devProjects: z
+    .array(
+      z.object({
+        projectId: z.string().min(1, "O ID do projeto é obrigatório"),
+      })
+    )
+    .optional(),
+});
+
+export const createProjectSchema = z.object({
+  name: z.string().min(1, "Nome do Projeto é obrigatório"),
+  description: z.string().min(1, "Descrição do Projeto é obrigatória"),
+  url: z.string().min(1, "URL é obrigatória"),
+  devProjects: z
+    .array(
+      z.object({
+        devId: z.string().min(1, "O ID do desenvolvedor é obrigatório"),
+      })
+    )
+    .min(1, "Pelo menos um desenvolvedor é obrigatório"),
+});
+
+export const updateProjectSchema = z.object({
+  name: z.string().min(1, "Nome do Projeto é obrigatório").optional(),
+  description: z.string().min(1, "Descrição do Projeto é obrigatória").optional(),
+  url: z.string().min(1, "URL é obrigatória").optional(),
+  devProjects: z
+    .array(
+      z.object({
+        devId: z.string().min(1, "O ID do desenvolvedor é obrigatório"),
+      })
+    )
+    .optional(),
+});
