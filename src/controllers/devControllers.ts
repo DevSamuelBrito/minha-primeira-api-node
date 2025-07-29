b385ba68ddcd8531ab5a9542849dc3c10507978f
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-07-29 15:11:55 -0300

    refactor: remove blocos trycatch e erros do controller

--- src/controllers/devControllers.ts
+++ src/controllers/devControllers.ts
@@ -6,58 +6,38 @@ import {
   updateDevSchema,
   updateProjectSchema,
 } from "../schemas/devSchema";
+import { ValidationError } from "../errors/ValidationError";
 
 export const getAllDevs: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
-  try {
-    const devs = await devService.getAllDevs();
-    res.json(devs);
-  } catch (error) {
-    res.status(500).json({ error: "Erro interno do servidor" });
-  }
+  const devs = await devService.getAllDevs();
+  res.json(devs);
 };
 
 export const getDevById: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
-  try {
-    const dev = await devService.getAllDevsByID(req.params.id);
-    if (!dev) {
-      res.status(404).send("Dev não encontrado");
-      return;
-    }
-    res.json(dev);
-  } catch (error) {
-    res.status(500).json({ error: "Erro interno do servidor" });
-  }
+  const dev = await devService.getAllDevsByID(req.params.id);
+  res.json(dev);
 };
 
 export const getAllProject: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
-  try {
-    const projects = await devService.getAllProjects();
-    res.json(projects);
-  } catch (error) {
-    res.status(500).json({ error: "Erro interno do servidor" });
-  }
+  const projects = await devService.getAllProjects();
+  res.json(projects);
 };
 
 export const getProjectById: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
-  try {
-    const project = await devService.getProjectByID(req.params.id);
-    if (!project) res.status(404).send("Projeto não encontrado");
-    res.json(project);
-  } catch (error) {
-    res.status(500).json({ error: "Erro interno do servidor" });
-  }
+  const project = await devService.getProjectByID(req.params.id);
+  res.json(project);
 };
 
 export const createDev: RequestHandler = async (
@@ -67,19 +47,11 @@ export const createDev: RequestHandler = async (
   const validation = createDevSchema.safeParse(req.body);
 
   if (!validation.success) {
-    res.status(400).json({
-      error: "Dados inválidos",
-      details: validation.error.errors,
-    });
-    return;
+    throw new ValidationError("Dados inválidos");
   }
 
-  try {
-    const dev = await devService.createDev(validation.data);
-    res.status(201).json({ message: "Dev criado com sucesso", dev });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
+  const dev = await devService.createDev(validation.data);
+  res.status(201).json({ message: "Dev criado com sucesso: ", dev });
 };
 
 export const createProject: RequestHandler = async (
@@ -89,26 +61,17 @@ export const createProject: RequestHandler = async (
   const validation = createProjectSchema.safeParse(req.body);
 
   if (!validation.success) {
-    res.status(400).json({
-      error: "Dados inválidos",
-      details: validation.error.errors,
-    });
-    return;
+    throw new ValidationError("Dados inválidos");
   }
 
   if (validation.data.devProjects.length === 0) {
-    res.status(400).json({
-      error: "Deve haver pelo menos um desenvolvedor vinculado ao projeto",
-    });
-    return;
+    throw new ValidationError(
+      "Deve haver pelo menos um desenvolvedor associado ao projeto"
+    );
   }
 
-  try {
-    const project = await devService.createProject(validation.data);
-    res.status(201).json({ message: "Projeto criado com sucesso", project });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
+  const project = await devService.createProject(validation.data);
+  res.status(201).json({ message: "Projeto criado com sucesso", project });
 };
 
 export const updateDev: RequestHandler = async (
@@ -118,22 +81,14 @@ export const updateDev: RequestHandler = async (
   const validation = updateDevSchema.safeParse(req.body);
 
   if (!validation.success) {
-    res.status(400).json({
-      error: "Dados inválidos",
-      details: validation.error.errors,
-    });
-    return;
+    throw new ValidationError("Dados inválidos");
   }
 
-  try {
-    const dev = await devService.updateDev(req.params.id, validation.data);
-    res.json({
-      message: "Dev atualizado com sucesso",
-      dev,
-    });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
+  const dev = await devService.updateDev(req.params.id, validation.data);
+  res.json({
+    message: "Dev atualizado com sucesso",
+    dev,
+  });
 };
 
 export const updateProject: RequestHandler = async (
@@ -143,45 +98,29 @@ export const updateProject: RequestHandler = async (
   const validation = updateProjectSchema.safeParse(req.body);
 
   if (!validation.success) {
-    res.status(400).json({
-      error: "Dados inválidos",
-      details: validation.error.errors,
-    });
-    return;
+    throw new ValidationError("Dados inválidos");
   }
-  try {
-    const project = await devService.updateProject(
-      req.params.id,
-      validation.data
-    );
-    res.json({
-      message: "Projeto atualizado com sucesso",
-      project,
-    });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
+  const project = await devService.updateProject(
+    req.params.id,
+    validation.data
+  );
+  res.json({
+    message: "Projeto atualizado com sucesso",
+    project,
+  });
 };
 
 export const deleteDev: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
-  try {
-    const result = await devService.deleteDev(req.params.id);
-    res.json({ message: "Dev deletado com sucesso", result });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
+  const result = await devService.deleteDev(req.params.id);
+  res.json({ message: "Dev deletado com sucesso", result });
 };
 export const deleteProject: RequestHandler = async (
   req: Request,
   res: Response
 ) => {
-  try {
-    const result = await devService.deleteProject(req.params.id);
-    res.json({ message: "Projeto deletado com sucesso", result });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
-  }
+  const result = await devService.deleteProject(req.params.id);
+  res.json({ message: "Projeto deletado com sucesso", result });
 };
